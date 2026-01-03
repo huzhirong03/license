d1db1b7bd39b88025e249cf6ef70caa7cca8e45a
huzhirong03 <379705723@qq.com>
2026-01-03 08:26:47 +0800

    Update keys.js

--- 0手机注册机激活/js/keys.js
+++ 0手机注册机激活/js/keys.js
@@ -8,8 +8,8 @@ const ACCESS_PASSWORD = '150904';
 
 // ==================== R2V 配置 ====================
 // AES 密钥（与 C# 版本一致）
-const LICENSE_FILE_KEY = 'FaGe2024LicenseKey!@#$%^&*()_+';
-const LICENSE_FILE_IV = 'FaGeLaser2024IV!';
+const LICENSE_FILE_KEY = 'fage_r2v_license_2024_aes256';
+const LICENSE_FILE_IV = 'fage_iv_16bytes!';
 
 // RSA 私钥（从 private.key 复制）
 const RSA_PRIVATE_KEY_XML = `<RSAKeyValue><Modulus>3dDMo1XJOQzvGNBdR4An3syRxggaHPCYQfn2XUo9uDyaN8lvvpGqLBKZRGX/LgKNmwm/hvLBO5IUIuXywRhJlcE3EW/cnDabfKzsIp+7/Z/p8t08tf+kuYawgtqSjZB3q3rtxEXT2McCGWY8weIwqkpMDWdxWJvvk3pmM0tySJZPbC3TIHN/PEjmdXj3ZznqsjWE0Yae9/y0oBOyjQZl+dhpL2mUqfkiG0xfhTMkWTnEdaOMpDSPSEO/rXK8sHdrgbvbxcPtMm2p0N5Wdto/iuvTP/pMZ2rD9eDPPDGV2L6/2p6y/u0lu4l9L7HnX8EfrafgvdFimVt79EtJeCD0HQ==</Modulus><Exponent>AQAB</Exponent><P>/3QCRnTpTeMGsZsg1vXJEZ8hwZ/4zqSzNB9HYhfJBdXVDfq8wykGjMeULLcf/EECHUt3AqeXSKiiC+AbLt2mdGtgU7Dw2wqw62E33o2DCR5bwCkvE2NIs71UEw5ULiTZ9+p/PiGCzKmZVKlNjIs8DbUucWGtIm/q7bsFeOd8LO8=</P><Q>3kpbU4TmAER7NFePIX3iL0AH3MnuSmOQmqCvV9YKniIblGI5Oirc2xll7CJZKZ4K1m3R+tsKSk6pqAef1nSHDKJuxYks2e7PZfW9M8JkMHejG5iBuSKsVngBgwt3ubApk2wFs/sy0ayjW/uHzEc02z8p17J2lTw9fEjZh+AdB7M=</Q><DP>a9Z7R2b/PkQChWNpX5VuhiPZSjXQANwCUpSJu90ynWYBhN8+ZFyjSTbPVHkR5ZlD3T4IRXSeJqwZjE3U69nh9O+HQI1HgBBpU1Q9E5RFpsnMDbjVCKg2/rmEDm9UFg60fTXe9UmMP/2wns7Bz/zPIzJDItYLEqiQUj9LNhjykL0=</DP><DQ>iBOuun71qZ5CabVcGl2Mc+5XodI/vGwc3nuVO0j9MPaiOYCzlacdkkaG/Tm9qVtlOQT7cPxb3UcwVntsXoTxE0vwUZ56xWKKtdjsHN5YJM9oGwGOD/5oz3ohOs5vWU289dE/4IDbCD4NtsZQyD2i3sBY5gEK1KqO3SFfgO0x+D8=</DQ><InverseQ>mpVEE0l/qlVKUadPQPMYwYs/io0xM8DECdiA08e3INk/8pD+FgOYE+1petzAbJOsE3bxUh/1cRZ7Sq2QcgzdGNxH5a1ksgT8mTilNQs6YO5TK2kH9/4erTIIaC/IfIpzxzF+1uH/RgWBkFtkyTfpgYxUiWH7YFTQs3jtTUX1C1s=</InverseQ><D>CAxtgHe6Z3GKs3HMEK4bxD/UAkWE/QvV2n8Ba30oJCk+x0tt3JJYhGTgXtT8n2TEUp3V70WbsVepl2KUnmENOnvaTa2raZepqKp1nwQAxEErMf7JFymBhqHWgckAjFZzoStCuV53Q2RlNRq6laZpcaRkghAA0uBFnypPzwiryupn/ZCSok5O4Zwe7p/wcFH644eJArWNHJeu76TdeJlU5NDOLBC5qwv6x+b4XX7QNwqtQEhg7SzyLtXxR2lVI/y2nyI2JO8RFy+1iCborCkhTDPU/yYXl8y3wr7Kge8Ck2wVib2NfCfbp9dTjK5YFdSVR8AVlriQfS7fNcQYFx+12Q==</D></RSAKeyValue>`;
